f2634410497c4830bfa359d633f6c6dc41a0bbbd
hyejeongjang <hj970219@naver.com>
2021-01-12 21:42:13 +0900

    first commit

---
+++ src/main/java/com/example/SongForyou/SongforyouApplication.java
@@ -0,0 +1,13 @@
+package com.example.SongForyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongforyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongforyouApplication.class, args);
+	}
+
+}
